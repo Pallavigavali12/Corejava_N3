dd947c2078178c23d615d391924665e26b37281a
tusha <tusha@LAPTOP-F25PKNED>
2023-02-01 13:57:15 +0530

    First Commit

---
+++ dayOne/src/org/tnsif/intro/SimpleJavaProgramDemo.java
@@ -0,0 +1,13 @@
+package org.tnsif.intro;
+
+public class SimpleJavaProgramDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to Java Fullstack Program!!");
+		
+		
+
+	}
+
+}
